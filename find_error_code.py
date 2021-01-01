d18001597aa5c1bf9d1c792f6eeaf109813d8b9d
Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>
2021-01-01 19:21:12 +0530

    Module to find the error code from output files added

---
+++ find_error_code.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+# jobid = "JOB00091"
+jes_files = []
+
+
+def find_jes_files(dir_path):
+    for item in dir_path.iterdir():
+        if item.is_dir():
+            find_jes_files(item)
+        elif item.is_file():
+            if item.name == "JESYSMSG.txt":
+                jes_files.append(item)
+        else:
+            print(item.name)
+
+
+def get_error_code(jobid):
+    path = Path("output/" + jobid)
+    find_jes_files(path)
+    # print(jes_files)
+    for jes_file in jes_files:
+        with open(jes_file) as jes:
+            for line in jes:
+                for word in line.split():
+                    if word.startswith("I") and word.isalnum() and not word.isalpha():
+                        return word
