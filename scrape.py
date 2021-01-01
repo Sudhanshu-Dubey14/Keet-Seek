1c417bfe89c8c1ee3cc714c91b76199704ec76a5
Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>
2021-01-01 19:19:56 +0530

    Code encapsulated in function and outputs added

--- scrape.py
+++ scrape.py
@@ -7,37 +7,41 @@ try:
 except ImportError:
     print("No module named 'google' found")
 
-# to search
-query = "ibm IEFC622I"
 
-for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-    if j.startswith("https://www.ibm.com/support/knowledgecenter"):
-        print(j)
-        link = j
-        break
-os.environ['MOZ_HEADLESS'] = '1'
-browser = webdriver.Firefox()
-browser.get(link)
-time.sleep(5)
-main_page = browser.page_source
-main_soup = BeautifulSoup(main_page, 'lxml')
-iframe = main_soup.find("iframe", class_="kc-iframe")
-print(iframe)
-browser.switch_to.frame(iframe["name"])
-html = browser.page_source
+def fetch_help(error_code):
+    # to search
+    query = "ibm " + error_code
 
-# page = requests.get(link)
-"""
-with open("html_file.html") as html:
-    page = html.read()
-"""
-# print(html)
-soup = BeautifulSoup(html, 'lxml')
-body = soup.find("div", class_="msgBody")
-print(body)
-for child in body.descendants:
-    if child.name == "h4":
-        print("\n" + child.string)
-    elif child.name == 'p':
-        print(child.string.rjust(80))
-browser.close()
+    print("Searching web for error code: " + error_code, end="\r")
+    for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+        if j.startswith("https://www.ibm.com/support/knowledgecenter"):
+            print(j)
+            link = j
+            break
+    os.environ['MOZ_HEADLESS'] = '1'
+    print("Loading web page: " + str(link), end="\r")
+    browser = webdriver.Firefox()
+    browser.get(link)
+    time.sleep(5)
+    main_page = browser.page_source
+    main_soup = BeautifulSoup(main_page, 'lxml')
+    iframe = main_soup.find("iframe", class_="kc-iframe")
+    print(iframe)
+    browser.switch_to.frame(iframe["name"])
+    html = browser.page_source
+
+    # page = requests.get(link)
+    """
+    with open("html_file.html") as html:
+        page = html.read()
+    """
+    # print(html)
+    soup = BeautifulSoup(html, 'lxml')
+    body = soup.find("div", class_="msgBody")
+    print(body)
+    for child in body.descendants:
+        if child.name == "h4":
+            print("\n" + child.string)
+        elif child.name == 'p':
+            print(child.string.rjust(80))
+    browser.close()
