771849876b07efa364b0e42b7b429567944056aa
Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>
2021-01-13 17:29:27 +0530

    Output Improved

--- jobs.py
+++ jobs.py
@@ -21,15 +21,15 @@ if args.type == "mainframe":
     job = my_jobs.submit_from_mainframe(args.path)
 elif args.type == "local":
     job = my_jobs.submit_from_local_file(args.path)
-print(job["phase-name"], end='\r')
+print(job["phase-name"].ljust(80, ' '), end='\r')
 i = 0
 while job["retcode"] is None:
     status = my_jobs.get_job_status(job["jobname"], job["jobid"])
     if status["retcode"] is not None:
-        print("Job processing is complete.")
+        print("Job processing is complete.".ljust(80, ' '))
         break
     if i == 1:
-        print("Waiting for job to complete.", end='\r')
+        print("Waiting for job to complete.".ljust(80, ' '), end='\r')
     elif i == 2:
         print("Waiting for job to complete..", end='\r')
     elif i == 3:
@@ -40,9 +40,10 @@ while job["retcode"] is None:
 if status["retcode"] == "CC 0000":
     print("Job was successfully completed!!!")
 else:
-    print(status)
+    print("Return Code: " + status["retcode"])
     print("Downloading Job output files for analysis...", end='\r')
+    print("".ljust(80, ' '), end="\r")
     subprocess.call(["zowe", "jobs", "download", "output", str(job["jobid"])])
     error_code = get_error_code(job["jobid"])
-    print("Error Code: " + str(error_code))
+    print("Possible Error Code: " + str(error_code))
     fetch_help(error_code)
